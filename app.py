8dad5c456e9291ed487b18cc9de497f7fca3afe5
Jonathan Ellis <jbellis@datastax.com>
2023-07-02 13:45:38 -0500

    wire up save_if_article

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
+from cassandra.cluster import Cluster
 from flask import Flask, request, jsonify
+
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).parent))
 import logic
 
+from db import DB
 
+
+db = DB(Cluster())
 app = Flask(__name__)
 
-@app.route('/is_article', methods=['POST'])
-def is_article():
+@app.route('/save_if_article', methods=['POST'])
+def save_if_article():
     data = request.json
-    content = data.get('content')
 
-    if not content:
-        return jsonify({"error": "Content not provided"}), 400
+    html_content = data.get('html_content')
+    url = data.get('url')
+    user_id_str = data.get('user_id')
 
-    result = logic.is_article(content)
+    if not html_content:
+        return jsonify({"error": "html_content not provided"}), 400
+    if not url:
+        return jsonify({"error": "url not provided"}), 400
+    if not user_id_str:
+        return jsonify({"error": "user_id not provided"}), 400
 
-    return jsonify({"is_article": result}), 200
+    result = logic.save_if_article(db, html_content, url, user_id_str)
+
+    return jsonify({"saved": result}), 200
 
 
 if __name__ == "__main__":
